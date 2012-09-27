317fc19897e09542cbc436a2efc306e7fbdc78a9
Sara <sara.bainbridge@gmail.com>
2012-09-27 15:59:08 -0700

    Mobile responsiveness added but width is weird

--- digitalu/js/common.js
+++ digitalu/js/common.js
@@ -1,12 +1,13 @@
 $(document).ready( function(){
 	$('.content').load('templates/what.html');
+	$('#nav-what').addClass('active');
 });
 
 
 
 $('.nav-link').live('click', function(){
-	var id = $(this).attr('id').split('-');
+	$('.active').removeClass('active');
+	var id = $(this).parent().addClass('active').attr('id').split('-');
 	id = id[1];
-	$('.content').load('templates/' + id + '.html');
-	
+	$('.content').load('templates/' + id + '.html');	
 });
