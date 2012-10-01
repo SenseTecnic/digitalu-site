32968123dc3fd3c42bd035595c13432814158597
Sara <sara.bainbridge@gmail.com>
2012-10-01 11:12:58 -0700

    Clunky fix to get it back to where it was.

--- digitalu/js/common.js
+++ digitalu/js/common.js
@@ -1,8 +1,8 @@
 $(document).ready( function(){
 	
-	if (isTouchDevice()){
-		initSlider();
-	}
+//	if (isTouchDevice()){
+//		initSlider();
+//	}
 	
 	$('.content').load('templates/what.html');
 	$('#nav-what').addClass('active');
