bf49f003796cd85e78e270ab7d76aceeb25caa10
Sara <sara.bainbridge@gmail.com>
2012-10-31 18:01:20 -0700

    More work on form.

--- digitalu/js/common.js
+++ digitalu/js/common.js
@@ -1,60 +1,132 @@
 //TODO - remove after dev
-$.ajaxSetup ({
-    // Disable caching of AJAX responses
-    cache: false
+$.ajaxSetup({
+	// Disable caching of AJAX responses
+	cache : false
 });
 
-$(document).ready( function(){
+$(document).ready(function() {
 
 	$('#nav-what').addClass('active');
 	truncateText();
-	
+
 });
 
+$('#add-member').live(
+		'click',
+		function() {
 
-$('#add-member').live('click', function(){
+			var numMembers = parseInt($('#num-members').val()) + 1;
+
+			var memberItemString = [
+					'<div class="control-group member-group">',
+					'<label class="control-label" for="member-controls">Team member #'
+							+ numMembers + '</label>',
+					'<div class="controls member-controls">',
+					'<div class="member-name-wrapper">',
+					'<input type="text" class="input-name" name="inputName-'
+							+ numMembers + '" placeholder="Name"></div>',
+					'<div class="member-email-wrapper">',
+					'<input type="text" class="input-email" name="inputEmail-'
+							+ numMembers + '" placeholder="Email"></div>',
+					'</div></div>' ].join('\n');
+			$('.member-group-wrapper').append(memberItemString);
+			$('#num-members').val(numMembers);
+			return false;
+		});
+
+function validateForm() {
 	
-	var numMembers = parseInt($('#num-members').val()) + 1;
+	var projectName = $('#projectName').val();
+	
+	if (!projectName){
+		alert("ERROR")
+		return false;
+	}
+		
 	
 	
-	var memberItemString =  ['<div class="control-group member-group">',
-	                         '<label class="control-label" for="member-controls">Team member #' + numMembers + '</label>',
-	                         '<div class="controls member-controls">',
-	                         '<div class="member-name-wrapper">',
-	                         '<input type="text" class="input-name" name="inputName-' + numMembers + '" placeholder="Name"></div>',
-	                         '<div class="member-email-wrapper">',
-	                         '<input type="text" class="input-email" name="inputEmail-'+ numMembers +'" placeholder="Email"></div>',
-	                         '</div></div>'      
-	                        ].join('\n');
 	
-	//var memberFormItem = '<div class="control-group member-group"><label class="control-label" for="inputPassword">Team member #' + ++numMembers + '</label><div class="controls"><input type="text" id="inputPassword" placeholder="Name"><input type="text" id="inputEmail" placeholder="Email"></div> </div>';
-	$('.member-group-wrapper').append(memberItemString);
-	$('#num-members').val(numMembers);
+	return true;
+}
+
+var aresult;
+
+function submitForm() {
+	
+	var formStr = JSON.stringify($('#registration-form').serializeObject());
+	
+	$.ajax({
+		type: 'POST',
+		url: "email_form.php",
+		data: formStr, 
+		success: function(result){
+			
+			result = JSON.parse(result);
+			
+			if (result.success == "true"){
+				$('#registration-form').remove();
+			
+				var successText = ['<h2 class="pixel">Thank you!</h2>',
+				                   '<p>Thanks for registering! You will receive an email ',
+				                   'explaining the next steps for your team within the next couple of days.',
+				                   '(If you don\'t receive an email, please contact us at ',
+				                   '<a href="mailto:digitalu@magic.ubc.ca">digitalu@magic.ubc.ca</a>.</p>'].join('\n');
+				
+				$('.form-wrapper').removeClass('row').html(successText);
+			}
+			else alert("NO")
+	    }
+	});
+	
+	
+}
+
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+
+$("form").submit(function() {
+	
+	if (validateForm()){
+		submitForm();
+	}	
+	
 	return false;
 });
 
+$('.nav-link').live('click', function() {
 
-
-$('.nav-link').live('click', function(){
-	
 	var activeID = $('.active').attr('id').split('-');
-	activeID = activeID[1];	
+	activeID = activeID[1];
 	$('.active').removeClass('active');
-	$('#' + activeID).hide();	
-	
+	$('#' + activeID).hide();
+
 	var id = $(this).parent().addClass('active').attr('id').split('-');
 	id = id[1];
 	$('#' + id).show();
 });
 
-function truncateText(){
-	if ($(window).width() < 500){
+function truncateText() {
+	if ($(window).width() < 500) {
 		$("#nav-register").find('a').text("Reg.");
 		$('#form-message-mobile').show();
 		$('#registration-form').hide();
 	}
-	
-		
+
 	else {
 		$("#nav-register").find('a').text("Register");
 		$('#form-message-mobile').hide();
@@ -65,5 +137,3 @@ function truncateText(){
 $(window).resize(function() {
 	truncateText();
 });
-
-
